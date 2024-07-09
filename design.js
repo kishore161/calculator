b2878b62a531dabe33537de677eff9e7d7ed0bd9
kishore161 <kishorekalai534@gmail.com>
2024-07-09 18:35:54 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,31 @@
+function add() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let result = num1 + num2;
+    document.getElementById('result').innerText = result;
+}
+
+function subtract() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let result = num1 - num2;
+    document.getElementById('result').innerText = result;
+}
+
+function multiply() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let result = num1 * num2;
+    document.getElementById('result').innerText = result;
+}
+
+function divide() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    if (num2 !== 0) {
+        let result = num1 / num2;
+        document.getElementById('result').innerText = result;
+    } else {
+        document.getElementById('result').innerText = 'Error: Division by zero';
+    }
+}
